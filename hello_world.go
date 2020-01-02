bb7f8f42bb49c0b31d089c18520cebda739f6933
Cleidson Medeiros <cleidsonmedeiros14@gmail.com>
2020-01-02 15:59:04 -0300

    implements hello world

---
+++ hello_world.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	fmt.Println("Hello, World!!!")
+	fmt.Println("What is your name? ")
+	fmt.Scanln(&name)
+
+	fmt.Println("Ok,", name, "be very Welcome to my Projects :) ")
+
+}
