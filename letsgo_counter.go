35b567f70d5789576a2410dcff72f54e0d5bf06f
Cleidsonmedeiros <59423470+Cleidsonmedeiros@users.noreply.github.com>
2020-01-04 23:34:31 -0300

    Update letsgo_counter.go

--- letsgo_counter.go
+++ letsgo_counter.go
@@ -13,7 +13,7 @@ func main() {
 	if number > 1000 {
 		fmt.Println("This number is too large, can crash your computer")
 	} else {
-		fmt.Println("From 0 to", number, "jumping from", jump, "Numbers, Are these numbers:")
+		fmt.Println("From 0 to", number, "Jumping from", jump, "Numbers, Are these numbers:")
 		counter(number, jump)
 
 	}
